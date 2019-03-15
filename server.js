919f411badeb75ef27af2bdc1023d3647d5276ce
Scott Capps <capps.scott@gmail.com>
2019-03-15 14:58:58 -0700

    server logic added

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').connection;
@@ -13,13 +18,16 @@ mongoose
   .then( () => console.log( 'Connected to database' ) )
   .catch( err => console.log( err ) )
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // API routes 
 app.use("/users", require('./routes/api/users'));
 app.use("/tasks", require('./routes/api/tasks'));
 app.use("/profile", require('./routes/api/profile'));
-
-app.get('/', (rea,res) => res.send('hello, dawg'));
 
 const port = process.env.PORT || 5000;
 
