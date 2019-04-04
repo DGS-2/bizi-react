bf380827de2ea69ad3d4e1319a2e713a524f5a88
Scott Capps <capps.scott@gmail.com>
2019-04-04 14:29:29 -0700

    small cleanup

--- client/src/components/no-profile/NoProfile.js
+++ client/src/components/no-profile/NoProfile.js
@@ -5,9 +5,10 @@ class NoProfile extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="text-center">I noticed you don't have a profile setup</h1>
+        <h1 className="text-center text-white">Your Profile Hasn't Loaded</h1>
+        <h3 className="text-center text-white">If you have created a profile, please wait. Otherwise, click the link below.</h3>
         <div className="row no-gutters">
-          <Link className="btn btn-outline-primary btn-xl mx-auto" to="/create-profile" >Create One Now</Link>
+          <Link className="btn btn-outline-light btn-xl mx-auto" to="/create-profile" >Create One Now</Link>
         </div>        
       </div>
     )
